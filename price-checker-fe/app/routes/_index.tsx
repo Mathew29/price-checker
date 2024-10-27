ed6a78448c4ddab9960d03b9dea10a9fafcf22f9
Mathew Akre <mwakre29@gmail.com>
2024-10-26 22:33:44 -0700

    styling changes

--- price-checker-fe/app/routes/_index.tsx
+++ price-checker-fe/app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, ActionFunctionArgs } from "@remix-run/node";
+import { Form, Link, redirect } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,19 +8,32 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function action({ request}: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const username = formData.get("username")
+  const password = formData.get("password")
+  console.log({ username, password })
+  return redirect(`/homePage`)
+}
+
 export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+          <h1 className="leading text-6xl font-bold decoration-accent-1">
             My Price Tracking App
           </h1>
         </header>
         <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <p className="leading-6 text-gray-700 dark:text-gray-200">
+          <p className="leading-6 decoration-accent-1">
             Sign In
           </p>
+          <Form method="post">
+              <input name="username" type="text" />
+              <input name="password" type="password" />
+              <button type="submit">Submit</button>
+          </Form>
 
         </nav>
       </div>
