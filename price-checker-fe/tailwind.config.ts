19a2e362ebb2868ad5e3153a938a5001df79da14
Mathew Akre <mwakre29@gmail.com>
2024-10-26 22:33:18 -0700

    make background

--- price-checker-fe/tailwind.config.ts
+++ price-checker-fe/tailwind.config.ts
@@ -4,6 +4,18 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        accent:{
+          1: "hsl(var(--color-accent1) / <alpha-value>)",
+          2: "hsl(var(--color-accent2) / <alpha-value>)"
+        },
+        bkg: "hsl(var(--color-bkg) / <alpha-value>)",
+        content: "hsl(var(--color-content) / <alpha-value>)",
+      },
+      animation: {
+        "spin-slower": "spin 35s ease infinite",
+        "spin-slow": "spin 25s ease-in-out infinite reverse",
+      },
       fontFamily: {
         sans: [
           '"Inter"',
