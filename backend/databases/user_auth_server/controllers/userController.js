8ee8ead5c8604a0dc67ef5a403eb48b9fd6cb647
Mathew Akre <mwakre29@gmail.com>
2024-11-01 17:27:39 -0700

    remove user name

--- backend/databases/user_auth_server/controllers/userController.js
+++ backend/databases/user_auth_server/controllers/userController.js
@@ -4,18 +4,18 @@ const pool = require('../config/db');
 
 
 const registerUser = async (req, res) => {
-    const { username, password_hash, email } = req.body;
+    const { password_hash, email } = req.body;
 
     try {
-        const existingUser = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+        const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
         if (existingUser.rows.length > 0) {
-            return res.status(409).json({ message: 'User already exists' });
+            return res.status(409).json({ message: 'Email already exists' });
         }
 
         const hashedPassword = await bcrypt.hash(password_hash, 10);
         const result = await pool.query(
-            'INSERT INTO users (username, password_hash, email) VALUES ($1, $2, $3) RETURNING *',
-            [username, hashedPassword, email]
+            'INSERT INTO users (password_hash, email) VALUES ($1, $2) RETURNING *',
+            [hashedPassword, email]
         );
 
         res.status(201).json({ id: result.rows[0].id });
@@ -28,8 +28,8 @@ const registerUser = async (req, res) => {
 
 
 const loginUser = async (req, res) => {
-    const { username, password_hash } = req.body;
-    const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    const { email, password_hash } = req.body;
+    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
 
     if (result.rows.length > 0) {
         const user = result.rows[0];
