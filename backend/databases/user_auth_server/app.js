807107375cad329506ea408d68c8b012cda278da
Mathew Akre <mwakre29@gmail.com>
2024-11-03 16:49:09 -0800

    remove logging

--- backend/databases/user_auth_server/app.js
+++ backend/databases/user_auth_server/app.js
@@ -1,14 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(helmet());
-app.use(morgan('combined'));
 app.use(express.json());
 
 const allowedOrigin = process.env.ALLOWED_ORIGIN || 'http://localhost:5173';
